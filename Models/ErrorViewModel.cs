c622d6635582dd33fd90830a5bef66711068d9fd
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-07-10 23:34:37 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GenerateQRCodeASpNetCoreMVC6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/GenerateQRCodeModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GenerateQRCodeASpNetCoreMVC6.Models
+{
+    public class GenerateQRCodeModel
+    {
+        [Display(Name ="Enter QR Code Text")]
+        public string QRCodeText { get; set; }
+    }
+}
